6aece54211887a5cbfd7ea5cefbc695d42f5bd0c
Fuiyono <encuadrante@gmail.com>
2018-04-25 13:20:10 -0500

    index

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1 style="">IDENTIFICA TU POSTURA POLITICA </br> Y VE HACIA AFUERA</h1>
+    <h3>Responde a este quiz para saber con que canditados</h3>
+    </br>
+    <button type="button" onclick="window.location.href='/quiz/page2.php'" name="button">Entrar</button>
+
+  </body>
+</html>
---
+++ page2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+    <form class="form" action="/quiz/quiz.php" method="post">
+      <label for="[object Object]">Género</label><br>
+      <input type="text" name="" value=""><br>
+      <label for="[object Object]">Edad</label><br>
+      <input type="text" name="" value=""><br>
+      <label for="[object Object]">Estado</label><br>
+      <input type="text" name="" value=""><br>
+      <input type="submit" name="" value="Comenzar"><br>
+
+    </form>
+  </body>
+</html>
