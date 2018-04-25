df14e791487c4191f4ef786c2cbd3cf18733ba01
Fuiyono <encuadrante@gmail.com>
2018-04-25 17:31:26 -0500

    1.0.0

--- index.php
+++ index.php
@@ -3,12 +3,23 @@
   <head>
     <meta charset="utf-8">
     <title></title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/styles.css">
   </head>
   <body>
+    <header>
+      <div class="contenedor">
+
+      </div>
+    </header>
+    <div class="contenedor">
+    <article class="contenido">
     <h1 style="">IDENTIFICA TU POSTURA POLITICA </br> Y VE HACIA AFUERA</h1>
     <h3>Responde a este quiz para saber con que canditados</h3>
     </br>
-    <button type="button" onclick="window.location.href='/quiz/page2.php'" name="button">Entrar</button>
-
+    <button type="button" class="boton" onclick="window.location.href='/quiz/page2.php'" name="button">Entrar</button>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </article>
+    </div>
   </body>
 </html>
